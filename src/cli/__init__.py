af0e808e87d565fbfa9b3c69975a0a4a89a65939
GodEyeTee <116340030+GodEyeTee@users.noreply.github.com>
2025-05-12 12:15:35 +0700

    Update __init__.py

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,9 +1,3 @@
-"""
-CRYPPO CLI (Command Line Interface) Module
-
-คำสั่งเกี่ยวกับข้อมูล การเทรนโมเดล และการทดสอบย้อนหลัง
-"""
-
 from . import main
 from .commands import data_commands, train_commands, backtest_commands
 
